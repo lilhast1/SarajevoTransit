d3f43d57432a12a4cca7ea1d1911d7ad56a3ebbf
Tarik Hastor <thastor1@etf.unsa.ba>
2026-03-29 20:39:19 +0200

    parekolakucke

---
+++ moneyman/src/test/java/com/sarajevotransit/moneyman/MoneymanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarajevotransit.moneyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
