51ed51f38f15c8a59abf0258bcdf605e04e37aa9
Edin Živojević <ezivojevic1@etf.unsa.ba>
2026-05-18 20:02:07 +0200

    Nesta lombok zeza

--- routingservice/src/main/java/ba/unsa/etf/pnwt/routingservice/messaging/TimetableEventPublisher.java
+++ routingservice/src/main/java/ba/unsa/etf/pnwt/routingservice/messaging/TimetableEventPublisher.java
@@ -2,15 +2,17 @@ package ba.unsa.etf.pnwt.routingservice.messaging;
 
 import ba.unsa.etf.pnwt.routingservice.config.RabbitMQConfig;
 import ba.unsa.etf.pnwt.routingservice.event.TimetableChangedEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class TimetableEventPublisher {
 
     private final RabbitTemplate rabbitTemplate;
+
+    public TimetableEventPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     public void publishTimetableChanged(TimetableChangedEvent event) {
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, RabbitMQConfig.ROUTING_TIMETABLE_CHANGED, event);
--- routingservice/src/main/java/ba/unsa/etf/pnwt/routingservice/messaging/TimetableNotificationResultListener.java
+++ routingservice/src/main/java/ba/unsa/etf/pnwt/routingservice/messaging/TimetableNotificationResultListener.java
@@ -4,19 +4,23 @@ import ba.unsa.etf.pnwt.routingservice.config.RabbitMQConfig;
 import ba.unsa.etf.pnwt.routingservice.event.TimetableChangedEvent;
 import ba.unsa.etf.pnwt.routingservice.event.TimetableNotificationFailedEvent;
 import ba.unsa.etf.pnwt.routingservice.event.TimetableNotificationSentEvent;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
-@Slf4j
 @Component
-@RequiredArgsConstructor
 public class TimetableNotificationResultListener {
 
+    private static final Logger log = LoggerFactory.getLogger(TimetableNotificationResultListener.class);
+
     private final TimetableEventPublisher publisher;
+
+    public TimetableNotificationResultListener(TimetableEventPublisher publisher) {
+        this.publisher = publisher;
+    }
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NOTIFICATION_SENT)
     public void onNotificationSent(TimetableNotificationSentEvent event) {
